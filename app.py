8b1d62c4f282bf0f32eb7746633098aa3837c81f
Lee Pak Shuang <81917538+pakshuang@users.noreply.github.com>
2023-08-19 22:40:01 +0800

    Add port to .env

--- app.py
+++ app.py
@@ -259,4 +259,4 @@ def consume_vial(coffee_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0", port=os.getenv("PORT"))
